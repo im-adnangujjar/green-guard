704c6b5fc0a195e6da7ce7bb1a1109e3592d2acd
im-adnangujjar <adnan.yaqoob.gujar@gmail.com>
2022-04-20 11:07:07 +0100

    Navbar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,26 +1,68 @@
-import React from 'react'
+import { useState } from 'react'
 import styles from "../../../style/navbar.module.css"
 import { Link } from '@chakra-ui/react'
+import { HamburgerIcon,CloseIcon } from '@chakra-ui/icons'
 
 function Navbar() {
+  const [show, setShow] = useState(false)
+
+  const onclickHandler = (() => {
+    setShow(!show)
+  })
+
+ 
+
+
   return (
     <div className={styles.display}>
-        <div>
-            <img src ={"/Logo.png"} alt ="logo" />
+      <div>
+        <img src={"/Logo.png"} alt="logo" />
+      </div>
+
+      <ul className={styles.list}
+      >
+        <Link href='/'>
+          <li className={styles.li}>Home</li>
+        </Link>
+        <Link href='/ourMission'>
+          <li className={styles.li}>Our Mission</li>
+        </Link>
+        <Link href='/about'>
+          <li className={styles.li}>About</li>
+        </Link>
+        <div className={styles.button}>download</div>
+      </ul>
+      {show? 
+      <div className={styles.icon}>
+        <div onClick={onclickHandler}>
+        <CloseIcon />
         </div>
-        <ul className={styles.list}
-        >
-          <Link href='/'>
-            <li className={styles.li}>Home</li>
-            </Link>
-            <Link href='/ourMission'>
-            <li className={styles.li}>Our Mission</li>
-            </Link>
-            <Link href='/about'>
-            <li className={styles.li}>About</li>
-            </Link>
-            <div className={styles.button}>download</div>
-        </ul>
+      </div>
+      :
+      <div  className={styles.icon}>
+        <div onClick={onclickHandler}><HamburgerIcon /></div>
+        </div>
+}
+
+      {show?
+
+      <div>
+        <ul className={styles.listItem}
+    >
+      <Link href='/'>
+        <li className={styles.li}>Home</li>
+        </Link>
+        <Link href='/ourMission'>
+        <li className={styles.li}>Our Mission</li>
+        </Link>
+        <Link href='/about'>
+        <li className={styles.li}>About</li>
+        </Link>
+        <div className={styles.button}>download</div>
+    </ul>
+      </div>
+      :''
+}
     </div>
   )
 }

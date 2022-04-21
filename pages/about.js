de3009e52535127734d0c9dcffa505c2e9a73cf6
im-adnangujjar <adnan.yaqoob.gujar@gmail.com>
2022-04-21 11:04:07 +0100

    images

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,12 @@
 
 import React from 'react'
 import HeaderSection1 from "../src/pages/about/headerSection1/HeaderSection1"
-// import FooterSection2  from "../src/pages/about/footerSection/FooterSection2";
-
 export default function about() {
   
 
   return (
     <>
    <HeaderSection1/>
-   {/* <FooterSection2/> */}
   </>
   )
 }
--- src/pages/home/thirdSection/ThirdSection.js
+++ src/pages/home/thirdSection/ThirdSection.js
@@ -11,8 +11,10 @@ function ThirdSection() {
           <h1 className={styles.heading1}>Malvertising?</h1>
           <p className={styles.paragraphs}>Malvertising is a malicious cyber strategy that uses online advertising to spread malware. It can be difficult to detect because it’s frequently delivered by believable ad networks. Once clicked on, they can change the security access to your system, leaving your computer exposed to hackers and cybercriminals.</p>
         </div>
+        <div className={styles.imageDisplay}>
         <img className={styles.image}src={"/image5.png" } alt="image not found" /> </div>
-
+        </div>
+        
       <div className={styles.display}>
         <div >
           <h1 className={styles.heading}>Protection for You   </h1>
@@ -22,7 +24,9 @@ function ThirdSection() {
             you can browse threat-free 24/7. All while
             keeping your data 100% private, even from us!</p>
         </div>
+        <div className={styles.imageDisplay}>
         <img  className={styles.image}src={"/image6.png"} alt="image not found" />
+        </div>
       </div>
     </div>
   )
